3b8304f62c7575d1e3106bde3724dcc370b04a36
Aditya <desh.adi05@gmail.com>
2019-06-15 02:19:22 +0530

    Added new service: Deployment

--- fulfil/commands.sh
+++ fulfil/commands.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 celery worker --app=worker.worker.app --concurrency=1 --hostname=djangoapp@%h --loglevel=INFO &
-daphne -b 0.0.0.0 -p $PORT fulfil.asgi:application
+daphne -b 0.0.0.0 -p 8000 fulfil.asgi:application
 #gunicorn --chdir fulfil --bind :8000 fulfil.wsgi:application
 #python manage.py runserver 0.0.0.0:8000
 #daphne -b 0.0.0.0 -p 8001 app.asgi:channel_layer
